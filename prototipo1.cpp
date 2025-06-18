87c2e91a4058716411104a31bc76e84045c3a703
61239779 <61239779@continental.edu.pe>
2025-06-17 21:37:16 -0500

    Update prototipo1.cpp
    prototipo de los recorridos

--- prototipo1.cpp
+++ prototipo1.cpp
@@ -23,55 +23,11 @@ Nodo *nuevoNodo(int valor) {
 void insertarnodo(nodo *& arbol, int n)
 
 //preorden
-void PreOrden(nodo *arbol){
-	if(arbol == NULL){
-		return ;
-	}else{
-		cout<<arbol->dato<<" - ";
-		PreOrden(arbol->izq);
-		PreOrden(arbol->der);
-	}
-}
+void PreOrden(nodo *arbol)
 //inorden
-void inOrden(Nodo *arbol){
- if(arbol == NULL){
-  return;
- }
- else{
-  inOrden(arbol->izq);
-  cout<<arbol->dato<<" - ";
-  inOrden(arbol->der);
- }
-}
-
-
+void inOrden(Nodo *arbol)
 //postorden
-void PostOrden(nodo *arbol){
-	if(arbol == NULL){
-		return ;
-	}else{
-		PostOrden(arbol->izq);
-		PostOrden(arbol->der);
-		cout<<arbol->dato<<" - ";
-	}
-}
+void PostOrden(nodo *arbol)
 //mostrar
-int main(){
-
-return 0;
-}
-//Funcion para mostrar el arbol completo
-void mostrarArbol(nodo *arbol, int cont){
-    if(arbol == NULL){//Saber si el arbol esta vacio.
-        return;
-    }
-    else{//Si no esta vacio.
-        mostrarArbol(arbol->der,cont+1);//imprimiendo lado derecho.
-        for(int i=0 ; i<cont ; i++){
-            cout<<"   ";
-        }
-        cout<<arbol->dato<<endl;
-        mostrarArbol(arbol->izq,cont+1);//imprimiendo lado izquierdo.
-    }
-}
+void mostrarArbol(nodo *arbol, int cont)
 //definicion
