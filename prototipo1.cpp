0f03daefe31fd7f2d1e8f79406971fd213884da8
Gilberth02 <73800174@continental.edu.pe>
2025-06-17 21:21:29 -0500

    Update prototipo1.cpp
    Agregue la estructura que tendrá el nodo

--- prototipo1.cpp
+++ prototipo1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+struct Nodo {
+    int dato;
+    Nodo *izq;
+    Nodo *der;
+};
+
+Nodo *nuevoNodo(int valor) {
+    Nodo *nodo = new Nodo();
+    nodo->dato = valor;
+    nodo->izq = NULL;
+    nodo->der = NULL;
+    return nodo;
+}
+
 //prototipos de funciones
 int main(){
 
